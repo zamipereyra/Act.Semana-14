43be5e0bcb788ccf392ec5d925d602557768ec3c
zamipereyra <zampereyra12@gmail.com>
2024-10-15 11:04:33 -0300

    Archivos

---
+++ Actividad semana 14/script.js
@@ -0,0 +1,21 @@
+const btnlogin1 = document.getElementById("btnlogin");
+const btnlogin2 = document.getElementById("btnLogin2");
+const usuarios = [''];
+const persona = {};
+const nombre = document.getElementById("usuario").value.trim();
+const edad = document.getElementById("edad").value.trim();
+const email = document.getElementById("email").value.trim();
+const producto = document.getElementById("producto").value.trim();
+const precio = document.getElementById("precio").value.trim();
+const categoría = document.getElementById("cat").value.trim();
+const form1 = document.getElementById("loginForm");
+const form2 = document.getElementById("loginForm2");
+
+
+
+
+form1.addEventListener("submit", () =>{
+    event.preventDefault();
+
+    
+})
